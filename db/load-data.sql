fca184a21d21b156df48925f8fe3aa1042db9de4
Calder <calderhayes@gmail.com>
2016-08-18 17:26:37 -0500

    Added some basic scripts

---
+++ db/load-data.sql
@@ -0,0 +1,6 @@
+
+-- Clients
+INSERT INTO oauth_clients (client_id, client_secret, redirect_uri) VALUES ("darik-web-app", "NotASecret", "http://fake/");
+
+-- Users
+INSERT INTO oauth_users (username, password, first_name, last_name) VALUES ('teacherdemo', 'password', 'Teacher', 'Demo');
--- index.php
+++ index.php
@@ -3,20 +3,41 @@
 // error reporting (this is a demo, after all!)
 ini_set('display_errors',1);error_reporting(E_ALL);
 
+// Environment
+// TODO: Set this from outside somehow
+define("ENVIRONMENT", "DEV");
+
 // Configuration
 $dsn = 'mysql:dbname=oauth2_db;host=localhost';
 $username = 'root';
 $password = 'ubuntu';
 
+if (!array_key_exists('HTTP_POST', $_SERVER)) {
+    if (ENVIRONMENT === 'DEV') {
+        $_SERVER['HTTP_POST'] = 'http://localhost';
+    }
+    else {
+        throw new Exception('Must launch a production version from HTTP server');
+    }
+}
+
+// Load dependencies
 require __DIR__ . '/vendor/autoload.php';
 
 use OAuth2\Storage\Pdo;
-use OAuth2\Server;
+use OAuth2\Server as OAuth2Server;
 use OAuth2\GrantType\RefreshToken;
-//use OAuth2\GrantType
+use OAuth2\GrantType\UserCredentials;
 
 $storage = new Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
 
-$server = new Server($storage);
+$grantTypes = array(
+    'user_credentials' => new UserCredentials($storage),
+    'refresh_token' => new RefreshToken($storage)
+);
+
+$server = new OAuth2Server($storage, array(
+    'issuer' => $_SERVER['HTTP_POST']
+), $grantTypes);
 
 
