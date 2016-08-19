ca9852158c5a55fdca482935c47e36e585a91dd7
Calder <calderhayes@gmail.com>
2016-08-18 20:01:26 -0500

    Added dummy certs

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ define("ENVIRONMENT", "DEV");
 $dsn = 'mysql:dbname=oauth2_db;host=localhost';
 $username = 'root';
 $password = 'ubuntu';
+$publicKey = file_get_contents('/home/ubuntu/oauth2/certs/dummy_rsa.pub');
+$privateKey = file_get_contents('/home/ubuntu/oauth2/certs/dummy_rsa');
 
 if (!array_key_exists('HTTP_POST', $_SERVER)) {
     if (ENVIRONMENT === 'DEV') {
@@ -33,14 +35,22 @@ use OAuth2\Response;
 
 $storage = new Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
 
+$keyStorage = new OAuth2\Storage\Memory(array('keys' => array(
+    'public_key' => $publicKey,
+    'private_key' => $privateKey
+)));
+
 /*$grantTypes = array(
     'user_credentials' => new UserCredentials($storage),
     'refresh_token' => new RefreshToken($storage)
 );*/
 
 $server = new OAuth2Server($storage, array(
-    'issuer' => $_SERVER['HTTP_POST']
+    'issuer' => $_SERVER['HTTP_POST'],
+    'use_openid_connect' => true
 ));//), $grantTypes);
+
+$server->addStorage($keyStorage, 'public_key');
 
 $scope = new OAuth2\Scope(array(
       'supported_scopes' => array('offline_access', 'api')
@@ -52,5 +62,7 @@ $server->addGrantType(new UserCredentials($storage));
 
 $request = Request::createFromGlobals();
 $response = new Response();
+
+header('Access-Control-Allow-Origin: *');
 
 $server->handleTokenRequest($request, $response) -> send();
--- scripts/access-token-test.sh
+++ scripts/access-token-test.sh
@@ -1,1 +1,2 @@
-curl -u darik-web-app:NotASecret http://localhost:8000/token -d 'grant_type=password&username=teacherdemo&password=password'
+curl -i -u darik-web-app:NotASecret http://localhost:8000/token -d 'grant_type=password&username=teacherdemo&password=password&scope=api+offline_access+openid'
+echo ""
